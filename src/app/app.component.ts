9082c1d55eacb0b51dfadfee93fa10e117f307e7
Kemal Yalcinkaya <kemalcany@gmail.com>
2017-03-13 09:40:00 +0000

    Frontend refactoring done in code, tests needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-
-export const views: Object[] = [
-  {
-    name: 'Dashboard',
-    icon: 'home',
-    link: ['']
-  },
-  {
-    name: 'Bad Link',
-    icon: 'error',
-    link: ['wronglink']
-  }
-];
-
+import { Content } from 'ionic-angular';
 
 @Component({
   selector: 'ion-app',
   template: `
     <ion-split-pane [ngClass]="screenWidth">
-      <ion-menu [content]="content" swipeEnabled="true">
-        <ion-header>
-          <ion-toolbar>
-            <ion-title>Menu</ion-title>
-          </ion-toolbar>
-        </ion-header>
-        <ion-content padding>
-          <ion-list>
-            <ion-item menuClose *ngFor="let view of views">
-              <a [routerLink]="view.link" routerLinkActive="active-link">
-                {{view.name}}
-              </a>
-            </ion-item>
-          </ion-list>
-        </ion-content>
+      <ion-menu [content]="mainContent" swipeEnabled="true">
+        <ais-menu></ais-menu>
       </ion-menu>
       <ais-header></ais-header>
-      <ion-content padding class="marginTopAdjusted">
-        <router-outlet #content></router-outlet>       
+      <ion-content #mainContent padding class="marginTopAdjusted">
+        <router-outlet></router-outlet>       
       </ion-content>    
     </ion-split-pane>
     <store-devtools *ngIf="showMonitor"></store-devtools>
@@ -50,8 +24,6 @@ export class AppComponent  {
 
   screenWidth = 'fullscreen';
 
-  views = views;
-
   constructor(
     public route: ActivatedRoute,
     public router: Router
@@ -59,6 +31,7 @@ export class AppComponent  {
     ( this.showMonitor ?
       this.screenWidth = 'width-adjusted-screen' :
       this.screenWidth = 'fullscreen');
+
   }
 
 }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,9 +2,11 @@
 import { Routes } from '@angular/router';
 
 import { DashboardPage } from './pages/dashboard.page';
+import { AboutPage } from './pages/about.page';
 import { NotFoundPage } from './pages/not-found.page';
 
 export const routes: Routes = [
   { path: '', component: DashboardPage, pathMatch: 'full' },
+  { path: 'about', component: AboutPage },
   { path: '**', component: NotFoundPage }
 ];
